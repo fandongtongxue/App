730636e0afa1908b7d7bc94e773bc1b0a914161d
fandong <admin@fandong.me>
2019-07-05 18:07:52 +0800

    Sort Code

--- App/AppDelegate.h
+++ App/AppDelegate.h
@@ -13,6 +13,7 @@
 @property (strong, nonatomic) UIWindow *window;
 
 @property (nonatomic, strong) NSData *deviceToken;
+
 - (UIViewController *)getLoginController;
 - (UITabBarController *)getMainController;
 
